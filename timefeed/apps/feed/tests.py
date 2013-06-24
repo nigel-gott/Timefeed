602d982c7befde309bac62170fca59803362bc42
Nigel Gott <nigel.gott@gmail.com>
2013-06-24 18:38:19 +0100

    Basic Feed test.

--- timefeed/apps/feed/tests.py
+++ timefeed/apps/feed/tests.py
@@ -6,5 +6,10 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from timefeed.apps.feed.models import Feed
 
-
+class FeedTest(TestCase):
+    def test_feed_model_name(self):
+        test_name = "TEST"
+        feed = Feed(name=test_name)
+        self.assertEquals(feed.name, test_name)
